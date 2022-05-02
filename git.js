9f2a8068646b33292225e44f0c48b8149f30e0c7
Jetdooo <wongsamud@gmail.com>
2022-05-02 23:36:56 +0700

    Test V 1.5

--- git.js
+++ git.js
@@ -2,8 +2,6 @@ console.log("Hello World Git");
 
 console.log("Hello git i need to test Program");
 
-<<<<<<< HEAD
 console.log(" Now I Save On Vscode");
-=======
+
 console.log("This log Save on github");
->>>>>>> d5fbfefb35418ffbad4179dd69387e6d063d8c76
